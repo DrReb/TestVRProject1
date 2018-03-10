664b5d3d0f1758ff15581018d23f375de486b056
DrReb <36999733+DrReb@users.noreply.github.com>
2018-03-10 18:57:56 +0300

    Initial commit

---
+++ Assets/Scripts/BehaviourScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BehaviourScript : MonoBehaviour
+{
+
+
+    public Material Gazed;
+    public Material NotGazed;
+    Renderer Target;
+
+    void Start()
+    {
+        Target = GetComponent<Renderer>();
+        Target.material = NotGazed;
+        LocalTeleportation();
+    }
+
+    public void GazeEvent()
+    {
+        Target.material = Gazed;
+    }
+
+    public void NotGazeEvent()
+    {
+        Target.material = NotGazed;
+    }
+
+    public void LocalTeleportation()
+    {
+        gameObject.transform.position = new Vector3(RandomPosition(), Random.Range(0.5f, 2), RandomPosition());
+    }
+
+    public float RandomPosition()
+    {
+        var coordinate = Random.Range(-9, 9);
+        while (coordinate > -2 && coordinate < 2)
+            coordinate = Random.Range(-9, 9);
+        return coordinate;
+    }
+}
---
+++ Assets/Scripts/ToVR.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VR;
+using UnityEngine.XR;
+
+public class ToVR : MonoBehaviour {
+
+	// Use this for initialization
+	public void Start ()
+    {
+        StartCoroutine(VROn("Cardboard"));
+	}
+
+    public IEnumerator VROn(string VR)
+    {
+        XRSettings.LoadDeviceByName(VR);
+        yield return null;
+        XRSettings.enabled = true;
+    }
+
+}
